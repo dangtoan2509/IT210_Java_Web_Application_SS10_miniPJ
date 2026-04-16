4bbada9a69330f6e961ea0ef7c19bf76b5787bfd
La The Phong <latp1102@gmail.com>
2026-04-17 00:41:59 +0700

    validator

--- src/main/java/com/session10miniproject/validator/ValidDateRange.java
+++ src/main/java/com/session10miniproject/validator/ValidDateRange.java
@@ -1,5 +1,14 @@
 package com.session10miniproject.validator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.*;
 
+@Documented
+@Constraint(validatedBy = ValidDateRangeValidator.class)
+@Target({ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ValidDateRange {
-
+    String message() default "Khoảng thời gian không hợp lệ";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
 }
