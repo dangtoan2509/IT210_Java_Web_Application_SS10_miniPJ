867bc17f9f980b4a715a03c358cf6933eeacc0f2
Quang Nguyen <nguyendangquang0510@example.com>
2026-04-16 23:36:15 +0700

    abc

--- src/main/java/com/session10miniproject/dto/BorrowRequestDTO.java
+++ src/main/java/com/session10miniproject/dto/BorrowRequestDTO.java
@@ -1,5 +1,106 @@
 package com.session10miniproject.dto;
 
+import com.session10miniproject.validator.CheckDateCollision;
+import com.session10miniproject.validator.ValidDateRange;
+import jakarta.validation.constraints.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@ValidDateRange
+@CheckDateCollision
 public class BorrowRequestDTO {
 
+    private String equipmentId;
+
+    @NotBlank(message = "Tên sinh viên không được để trống")
+    private String studentName;
+
+    @NotBlank(message = "Mã sinh viên không được để trống")
+    @Pattern(regexp = "^[a-zA-Z]{2}\\d+$", message = "Mã sinh viên phải bắt đầu bằng 2 chữ cái, theo sau là số")
+    private String studentId;
+
+    @NotBlank(message = "Email không được để trống")
+    @Email(message = "Email không đúng định dạng")
+    private String email;
+
+    @NotNull(message = "Số lượng không được để trống")
+    @Min(value = 1, message = "Số lượng phải lớn hơn hoặc bằng 1")
+    private Integer quantity;
+
+    @NotNull(message = "Ngày mượn không được để trống")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate borrowDate;
+
+    @NotNull(message = "Ngày trả không được để trống")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate returnDate;
+
+    @NotBlank(message = "Lý do mượn không được để trống")
+    private String reason;
+
+
+    public String getEquipmentId() {
+        return equipmentId;
+    }
+
+    public void setEquipmentId(String equipmentId) {
+        this.equipmentId = equipmentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public LocalDate getBorrowDate() {
+        return borrowDate;
+    }
+
+    public void setBorrowDate(LocalDate borrowDate) {
+        this.borrowDate = borrowDate;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
 }
